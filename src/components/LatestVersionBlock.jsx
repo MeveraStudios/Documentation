bc756cbb1219f4e564646229747ea806e7ac7dd5
Ahmed Waleed <61851106+iiAhmedYT@users.noreply.github.com>
2025-04-20 12:17:55 +0200

    Update LatestVersionBlock.jsx

--- src/components/LatestVersionBlock.jsx
+++ src/components/LatestVersionBlock.jsx
@@ -7,43 +7,32 @@ import LatestVersion from './LatestVersion';
 
 export default function LatestVersionBlock({ owner, repo, group, id }) {
   return (
-    <Tabs>
-      <TabItem value="maven" label="Maven">
-        <CodeBlock language="xml">
-          {[
-            `<dependency>\n`,
-            `  <groupId>${group}</groupId>\n`,
-            `  <artifactId>${id}</artifactId>\n`,
-            `  <version>`,
-            <LatestVersion key="maven-version" owner={owner} repo={repo} />,
-            `</version>\n`,
-            `</dependency>`
-          ]}
-        </CodeBlock>
-      </TabItem>
+    <LatestVersion owner={owner} repo={repo}>
+      {(v) => {
+        const maven = `<dependency>
+  <groupId>${group}</groupId>
+  <artifactId>${id}</artifactId>
+  <version>${v}</version>
+</dependency>`;
 
-      <TabItem value="gradle-groovy" label="Gradle (Groovy)">
-        <CodeBlock language="groovy">
-          {[
-            `implementation '`,
-            `${group}:${id}:`,
-            <LatestVersion key="groovy-version" owner={owner} repo={repo} />,
-            `'`
-          ]}
-        </CodeBlock>
-      </TabItem>
+        const groovy = `implementation '${group}:${id}:${v}'`;
+        const kotlin = `implementation("${group}:${id}:${v}")`;
 
-      <TabItem value="gradle-kotlin" label="Gradle (Kotlin)">
-        <CodeBlock language="kotlin">
-          {[
-            `implementation("`,
-            `${group}:${id}:`,
-            <LatestVersion key="kotlin-version" owner={owner} repo={repo} />,
-            `")`
-          ]}
-        </CodeBlock>
-      </TabItem>
-    </Tabs>
+        return (
+          <Tabs>
+            <TabItem value="maven" label="Maven">
+              <CodeBlock language="xml">{maven}</CodeBlock>
+            </TabItem>
+            <TabItem value="gradle-groovy" label="Gradle (Groovy)">
+              <CodeBlock language="groovy">{groovy}</CodeBlock>
+            </TabItem>
+            <TabItem value="gradle-kotlin" label="Gradle (Kotlin)">
+              <CodeBlock language="kotlin">{kotlin}</CodeBlock>
+            </TabItem>
+          </Tabs>
+        );
+      }}
+    </LatestVersion>
   );
 }
 
